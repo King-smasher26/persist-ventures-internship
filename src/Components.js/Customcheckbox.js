8e9bf4cba1b6bd499a63b55c3fcca9faeae76f3a
Aman <yoab100203@gmail.com>
2023-08-11 17:27:58 +0530

    changes in customcheckbox.js

--- src/Components.js/Customcheckbox.js
+++ src/Components.js/Customcheckbox.js
@@ -5,11 +5,23 @@ export default function Customcheckbox() {
     const newentry=[e.target.name,e.target.checked]
     console.log(newentry)
     // props.clicked(newentry);
-    console.log(newentry  )
+    console.log(newentry)
+    if(e.target.checked==true){
+      console.log("now");
+      const mystyle = {
+        backgroundColor: "DodgerBlue",
+      };
+      console.log(
+
+        document.querySelector(".walking-options").style.display="flex"
+        )
+    }
 }
   return (
     <div>
         <section className="app">
+          <div className='expand-checkbox'>
+
   <i className="fa fa-rocket"></i>
   <article className="feature1">
     <input onClick={clicked} type="checkbox" id="feature1" name='walking' />
@@ -19,7 +31,33 @@ export default function Customcheckbox() {
       </span>
     </div>
   </article>
-  
+  <div className='walking-options'>
+  <article className="feature3">
+    <input onClick={clicked} type="checkbox" id="feature3" name='walking_slow'/>
+    <div>
+      <span>
+        Slow
+      </span>
+    </div>
+  </article>
+  <article className="feature3">
+    <input onClick={clicked} type="checkbox" id="feature3" name='walking_medium'/>
+    <div>
+      <span>
+        Medium
+      </span>
+    </div>
+  </article>
+  <article className="feature3">
+    <input onClick={clicked} type="checkbox" id="feature3" name='walking_fast'/>
+    <div>
+      <span>
+        Fast
+      </span>
+    </div>
+  </article>
+  </div>
+          </div>
   <article className="feature2">
     <input onClick={clicked} type="checkbox" id="feature2" name='running' />
     <div>
@@ -43,6 +81,22 @@ export default function Customcheckbox() {
     <div>
       <span>
         Dogwalking
+      </span>
+    </div>
+  </article>
+  <article className="feature4">
+    <input onClick={clicked} type="checkbox" id="feature4" name='dogwalking' />
+    <div>
+      <span>
+        Gardening
+      </span>
+    </div>
+  </article>
+  <article className="feature4">
+    <input onClick={clicked} type="checkbox" id="feature4" name='dogwalking' />
+    <div>
+      <span>
+        Swimming
       </span>
     </div>
   </article>

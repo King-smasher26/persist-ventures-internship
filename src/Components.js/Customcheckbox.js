b20df0f140334da2b7ed0b369bdef7a4a5d3b27b
Aman <yoab100203@gmail.com>
2023-08-11 15:39:51 +0530

    some changes in customcheckbox.js

--- src/Components.js/Customcheckbox.js
+++ src/Components.js/Customcheckbox.js
@@ -1,66 +1,55 @@
 import React from 'react'
 import "../App.css"
 export default function Customcheckbox() {
+  function clicked(e){
+    const newentry=[e.target.name,e.target.checked]
+    console.log(newentry)
+    // props.clicked(newentry);
+    console.log(newentry  )
+}
   return (
     <div>
         <section className="app">
   <i className="fa fa-rocket"></i>
-  <h1>It's time to <span className="blue-color">upgrade</span></h1>
-  <h3 className="gray-color">Choose the feature that best fit your needs and you'll be good to go!</h3>
   <article className="feature1">
-    <input type="checkbox" id="feature1" />
+    <input onClick={clicked} type="checkbox" id="feature1" name='walking' />
     <div>
       <span>
-        20 GB<br/>
-        + $15.00
+        Walking
       </span>
     </div>
   </article>
   
   <article className="feature2">
-    <input type="checkbox" id="feature2" />
+    <input onClick={clicked} type="checkbox" id="feature2" name='running' />
     <div>
       <span>
-        10 Emails<br/>
-        + $10.00
+        Running
       </span>
     </div>
   </article>
   
   <article className="feature3">
-    <input type="checkbox" id="feature3"/>
+    <input onClick={clicked} type="checkbox" id="feature3" name='bicycling'/>
     <div>
       <span>
-        Support<br/>
-        + $65.00
+        bicycling
       </span>
     </div>
   </article>
   
   <article className="feature4">
-    <input type="checkbox" id="feature4" />
+    <input onClick={clicked} type="checkbox" id="feature4" name='dogwalking' />
     <div>
       <span>
-        Coffee<br/>
-        $1.00
+        Dogwalking
       </span>
     </div>
   </article>
   
-  <h3>
-    <span className="blue-color">50% </span>discount only today!
-  </h3>
   
-  <a href="#" className="upgrade-btn">Upgrade</a>
-  
-  <div className="gray-color">terms & conditions apply, the app will never share your private info</div>
 </section>
 
-<div className="social">
-  <a href="https://www.linkedin.com/in/florin-cornea-b5118057/" target="_blank">
-    <i className="fab fa-linkedin"></i>
-  </a>
-</div>
     </div>
   )
 }
